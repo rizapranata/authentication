7a731aabde906948a8a3fde9cc80120d2b6f506f
rizapranata <riza.ari@wgs-id.com>
2023-12-25 21:43:05 +0700

    accessing protected resource and add logout function

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const authCtx = useContext(AuthContext);
+  const [fetchMessage, setFetchMessage] = useState("");
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://expense-app-36c12-default-rtdb.asia-southeast1.firebasedatabase.app/messeage.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchMessage(response.data);
+      });
+  }, []);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchMessage}</Text>
     </View>
   );
 }
@@ -14,13 +32,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
