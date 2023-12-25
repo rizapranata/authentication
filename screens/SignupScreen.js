8f10d0f2ec4bcf4b2c44b97db48b1e62394bd246
rizapranata <riza.ari@wgs-id.com>
2023-12-25 20:25:16 +0700

    protecting screen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,23 +1,26 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { createUser } from "../util/auth";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [isAuthenticating, setAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
 
   async function signupHandler({ email, password }) {
     setAuthenticating(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed!",
         "Could not you in. Please check your credeantials or try again later!"
       );
+      setAuthenticating(false);
     }
-    setAuthenticating(false);
   }
 
   if (isAuthenticating) {
