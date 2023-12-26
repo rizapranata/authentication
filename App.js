68c74e853ebef195a94047f44a1bab264dd1be41
rizapranata <riza.ari@wgs-id.com>
2023-12-26 10:27:35 +0700

    implement local storage to save token

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import { Colors } from "./constants/styles";
 import AuthContextProvider, { AuthContext } from "./store/auth-context";
 import IconButton from "./components/ui/IconButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import AppLoading from "expo-app-loading";
 
 const Stack = createNativeStackNavigator();
 
@@ -66,12 +68,38 @@ function Navigation() {
   );
 }
 
+function Root() {
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchToken() {
+       const storedToken = await AsyncStorage.getItem('token');
+
+       if (storedToken) {
+        authCtx.authenticate(storedToken);
+       }
+
+       setIsTryingLogin(false)
+    }
+
+    fetchToken();
+  }, []);
+
+  if (isTryingLogin) {
+    return <AppLoading /> //deprecated
+  }
+
+  return <Navigation />
+}
+
 export default function App() {
+  
   return (
     <>
       <StatusBar style="light" />
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   );
